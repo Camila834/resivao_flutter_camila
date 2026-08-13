e1d89b74e65f6900c6b50fa2f0b63da6a6503e00
camila834 <camiladesouzaalves477@gmail.com>
2026-08-13 11:07:59 -0300

    Mais exemplos

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:resivao_flutter_camila/revisao/exemplo1.dart';
+import 'package:resivao_flutter_camila/revisao/exemplo2.dart';
+import 'package:resivao_flutter_camila/revisao/exemplo3.dart';
+import 'package:resivao_flutter_camila/revisao/exemplo4.dart';
+import 'package:resivao_flutter_camila/revisao/exemplo5.dart';
+import 'package:resivao_flutter_camila/revisao/exemplo6.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Exemplo1Page(),
+      home: Exemplo6Page(),
     );
   }
 }
---
+++ lib/styles/exemplo3_styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Exemplo3Styles {
+  static const Color carCard = Colors.blue;
+  static const Color carSombra = Colors.black26;
+
+  static const TextStyle textoCard = TextStyle(color: Colors.white);
+}
